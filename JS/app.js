90fd0e937289f64371cafe9f20ce31cf193d1b50
Tobias Foit <tobias.foit@gmail.com>
2023-10-09 19:30:11 +0100

    image and button text basic setup

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,14 @@
-// function userName() {
-//   let intro = prompt("What is your name?");
+// Function to handle form submission
+const nameForm = document.getElementById("nameForm");
+nameForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const messageDiv = document.getElementById("messageDiv");
+  messageDiv.textContent = "Hi there, " + name + "!";
+  // Reset the form
+  nameForm.reset();
+});
 
-//   alert("Hello " + intro);
-// }
-// userName();
 let userScore = [];
 let blotList = [];
 let currentStep = 0;
@@ -17,17 +22,18 @@ Blot.prototype.addToList = function () {
   blotList.push(this);
 };
 
-const blot1 = new Blot("./images/rorschach-blot-1.jpeg", [
+const blot1 = new Blot("./images/blots/rorschach-blot-1.jpeg", [
   ["bat", [0, 1, 0, 0, 1, 0]],
   ["moth", [0, 1, 0, 0, 0, 0]],
   ["butterfly", [0, 1, 0, 0, 0, 0]],
 ]);
-
-const blot2 = new Blot("./images/rorschach-blot-1.jpeg", [
-  ["bat", 0, 1, 1, 0, 0, 0],
-  ["penis", 0, 0, 0, 0, 0, 1],
-  ["house", 0, 1, 0, 0, 0, 0],
+blot1.addToList();
+const blot2 = new Blot("./images/blots/rorschach-blot-2.jpeg", [
+  ["chicken", [0, 1, 1, 0, 0, 0]],
+  ["penis", [0, 0, 0, 0, 0, 1]],
+  ["house", [0, 1, 0, 0, 0, 0]],
 ]);
+blot2.addToList();
 
 const userTraits = [
   "Creative",
@@ -40,14 +46,13 @@ const userTraits = [
 
 // show inkblot image in inkblotarea
 const inkblotarea = document.querySelector("#inkblotarea");
-inkblotarea.style.backgroundImage = `url("${blotList[0][0]}")`;
+inkblotarea.style.backgroundImage = `url("${blotList[0].src}")`;
 
 const button1 = document.querySelector("#button1");
-button1.textContent = `${interpretationsBlot1[0]}`;
-const button2 = document.querySelector("#button2");
-button1.textContent = `${interpretationsBlot1[1]}`;
-const button3 = document.querySelector("#button3");
-button1.textContent = `${interpretationsBlot1[2]}`;
+button1.textContent = `${blotList[0].options[0][0]}`;
+button2.textContent = `${blotList[0].options[1][0]}`;
+button3.textContent = `${blotList[0].options[2][0]}`;
+
 const skipbutton = document.querySelector("#skipbutton");
 
 button1.addEventListener("click", function () {
@@ -57,6 +62,3 @@ button1.addEventListener("click", function () {
 
   // show new prompts
 });
-
-blot2.addToList();
-blot1.addToList();
